8ca61624ef6ef4ccc91ab0b8079079e49de3ee75
Jacob Sifuentes <jacob@sifuen.com>
2024-11-07 19:15:38 -0500

    change output filename hash to include the host header to avoid conflicts

--- runner/runner.go
+++ runner/runner.go
@@ -1057,7 +1057,7 @@ func (r *Runner) RunEnumeration() {
 
 			// store responses or chain in directory
 			URL, _ := urlutil.Parse(resp.URL)
-			domainFile := resp.Method + ":" + URL.EscapedString()
+			domainFile := resp.Method + ":" + URL.EscapedString() + ":" + resp.RequestHostHeader
 			hash := hashes.Sha1([]byte(domainFile))
 			domainResponseFile := fmt.Sprintf("%s.txt", hash)
 			screenshotResponseFile := fmt.Sprintf("%s.png", hash)
